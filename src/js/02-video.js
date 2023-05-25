fc2fb8fd43b5421bc1d7615bcd448c5a822c4ef3
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-05-25 15:10:22 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+    // console.log('timeupdate the video!');
+    // console.log(data);
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
 
